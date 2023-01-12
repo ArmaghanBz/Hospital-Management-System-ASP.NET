ad256e37ad8ad198c257f86e654ffab238d76ed6
Armaghan Bin Zahid <armaghanbinzahid@gmail.com>
2023-01-12 09:41:27 +0500

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+using System.Web.Security;
+
+namespace WebApplication2.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult login()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult login(Models.Membership model)
+        {
+            using(var context = new HMSEntities1())
+            {
+                bool isValid = context.user.Any(x => x.userName == model.userName && x.password == model.password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.userName, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Invalid username and password");
+            }
+            return View();
+        }
+        public ActionResult signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult signup(user model)
+        {
+            using(var context = new HMSEntities1())
+            {
+                context.user.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("login");
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
---
+++ Controllers/DoctorsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2;
+
+namespace WebApplication2.Controllers
+{
+    [Authorize]
+    public class DoctorsController : Controller
+    {
+        private HMSEntities1 db = new HMSEntities1();
+
+        // GET: Doctors
+        public ActionResult Index()
+        {
+            return View(db.Doctor.ToList());
+        }
+
+        // GET: Doctors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctor doctor = db.Doctor.Find(id);
+            if (doctor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctor);
+        }
+
+        // GET: Doctors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Designation,Salary,Dept,Speciality,Age")] Doctor doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Doctor.Add(doctor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(doctor);
+        }
+
+        // GET: Doctors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctor doctor = db.Doctor.Find(id);
+            if (doctor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctor);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Designation,Salary,Dept,Speciality,Age")] Doctor doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(doctor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(doctor);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctor doctor = db.Doctor.Find(id);
+            if (doctor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctor);
+        }
+
+        // POST: Doctors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Doctor doctor = db.Doctor.Find(id);
+            db.Doctor.Remove(doctor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
